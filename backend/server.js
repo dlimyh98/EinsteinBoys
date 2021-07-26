a611d4c107c5f85af73367523ca2c78524c39e49
Damien <dlimyh98@hotmail.com>
2021-07-26 12:33:41 +0800

    fixing router

--- backend/server.js
+++ backend/server.js
@@ -156,11 +156,11 @@ app.listen(PORT, console.log(`Server running on ${PORT}`));
 
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
-  app.use(express.static('/build'));
+  app.use(express.static('build'));
 
   // Express serve up index.html file if it doesn't recognize route
   const path = require('path');
-  app.get('/', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve('/app', 'build', 'index.html'));
   });
 }
