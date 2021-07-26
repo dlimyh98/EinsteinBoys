854a30c931856789250bb5810088c7567da756cb
Damien <dlimyh98@hotmail.com>
2021-07-26 11:46:47 +0800

    added modifications to server.js, serving index.html

--- backend/server.js
+++ backend/server.js
@@ -153,3 +153,14 @@ app.put("/tasks", (req, res) => {
 // Backend server will using PORT specified below
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, console.log(`Server running on ${PORT}`));
+
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
+}
